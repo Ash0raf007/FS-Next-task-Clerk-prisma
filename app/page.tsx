c0dcec3218e258de749fec3c6008c58cdd5e555a
Ash0raf007 <Ashrafkadry07@gmail.com>
2024-12-27 22:40:57 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+"use server"
+import { getUserTodoListAction } from "@/actions/todoActions";
+import AddTodoForm from "@/components/AddTodoForm";
+import TodosTable from "@/components/TodoTable";
+import { auth } from "@clerk/nextjs";
+
+export default async function Home() {
+  const { userId } = auth();
+  const todos = await getUserTodoListAction({ userId });
+
+  return (
+    <main className="container">
+      <div className="mx-auto flex w-full lg:w-3/4 flex-col justify-center space-y-4 mt-10">
+        <AddTodoForm userId={userId} />
+        <TodosTable todos={todos} />
+      </div>
+    </main>
+  );
+}
